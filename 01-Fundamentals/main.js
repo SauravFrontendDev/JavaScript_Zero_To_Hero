a5b9b64a94cf753ab7d5fdb365168389ed4d697c
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-08-30 23:56:28 +0530

    learning javascript fundamentals

---
+++ 01-Fundamentals/main.js
@@ -0,0 +1,13 @@
+/**
+ * 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions).
+ * 2. Log their values to the consol
+
+ */
+
+let country = "India";
+let continent = "Asia";
+let population = 1428.62;
+
+console.log(country);
+console.log(continent);
+console.log(population);
